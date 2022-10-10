f46522ee6d35eb6d0e91f5a7243ecea0427d7460
Provorov Anton <provorovua@gmail.com>
2022-10-10 17:09:08 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,6 +1,6 @@
 const inputEl = document.querySelector('#validation-input');
 const handleInput = event => {
-  event.currentTarget.value.length == inputEl.dataset.length
+  event.currentTarget.value.length === Number(inputEl.dataset.length)
     ? inputEl.classList.replace('invalid', 'valid') || inputEl.classList.add('valid')
     : inputEl.classList.replace('valid', 'invalid') || inputEl.classList.add('invalid');
   console.log(event.currentTarget.value.length);
